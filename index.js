fb01855b7f4ff2252baffc40a05c09376092e96b
Sergio Neves <serneves@hotmail.com>
2018-11-02 22:59:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,9 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-    returns "I can't hear you!"
+    return "I can't hear you!"
   }
   else if (string.toUpperCase() === string) {
-
     return "YES INDEED!"
   }
   else {
